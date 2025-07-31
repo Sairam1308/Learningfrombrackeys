6a61063c5e52bdb760beb3f585d53720b13ad3d0
Sairam1308 <Sairam1308@gmail.com>
2025-07-31 21:59:41 +0530

    level design

--- Learningfrombrackeys/Assets/Scripts/Playermovement.cs
+++ Learningfrombrackeys/Assets/Scripts/Playermovement.cs
@@ -13,12 +13,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
