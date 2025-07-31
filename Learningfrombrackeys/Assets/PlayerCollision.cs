1644dfb119f66a341a23651b80b65718fe326591
Sairam1308 <Sairam1308@gmail.com>
2025-07-31 21:09:27 +0530

    camera movement and collision

---
+++ Learningfrombrackeys/Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        
+        if(collisionInfo.collider.tag=="Obstacle")
+        {
+            Debug.Log(collisionInfo.collider.name);
+            movement.enabled = false;
+        }
+
+    }
+}
